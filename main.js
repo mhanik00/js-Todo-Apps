bbbb6b6ddf3cf0af1fa3fddf1f4c4097fb8ac6e1
mhanik00 <mhanik063@gmail.com>
2024-03-28 16:30:05 +0600

    change the word warp

--- main.js
+++ main.js
@@ -3,43 +3,43 @@
 
 
 // *todo apps function
-function addTask(){
+function addTask() {
     // TASK input box
     const inputBox = document.getElementById("input-box");
     // TASK list
     const todoList = document.getElementById("todo-list");
     // TASk 
     const inputTask = inputBox.value.trim();
-    if(inputTask === ""){
+    if (inputTask === "") {
         alert("You must write something!");
 
         // *delete button for empty task
-        deleteButton.removeEventListener = function(){
-            
+        deleteButton.removeEventListener = function () {
+
         }
     }
     // *add task to list
-        const li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        // push li to ul
-        todoList.appendChild(li);
-        // * For clear input
-        inputBox.value = "";
-        
+    const li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    // push li to ul
+    todoList.appendChild(li);
+    // * For clear input
+    inputBox.value = "";
+
 
     // *delete button
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
 
-        // **create class for delete button
-        deleteButton.classList.add("delete-button");
+    // **create class for delete button
+    deleteButton.classList.add("delete-button");
 
-        // add delete button to li
-        li.appendChild(deleteButton);
-        deleteButton.addEventListener("click", function(){
-            li.remove();
-        })
-    
-    
-    }
-        
+    // add delete button to li
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener("click", function () {
+        li.remove();
+    })
+
+
+}
+
